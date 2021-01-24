38cecc9e4af9ad57e6aae77af6f7b0cb6901b69c
Loucasterix <73615087+Loucasterix@users.noreply.github.com>
2021-01-24 18:25:38 +0100

    UI

---
+++ Assets/Scripts/GameManager1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager1 : MonoBehaviour
+{
+
+    public void EnterLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -18,12 +19,18 @@ public class PlayerController : MonoBehaviour
 
     private Stopwatch m_stopwatch;
 
+    public Text scoreText;
+
+    public GameObject gameOverText;
+
 
     private void Start()
     {
         m_playerRigidbody = GetComponent<Rigidbody>();
 
         m_collectablesTotalCount = m_collectablesCounter = GameObject.FindGameObjectsWithTag("Collectable").Length;
+
+        scoreText.text = "Score: " + m_collectablesCounter.ToString() + " / " + m_collectablesTotalCount.ToString();
 
         m_stopwatch = Stopwatch.StartNew();
 
@@ -55,14 +62,17 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
 
             m_collectablesCounter--;
+            scoreText.text = "Score: " + m_collectablesCounter.ToString() + " / " + m_collectablesTotalCount.ToString();
+
             if (m_collectablesCounter == 0)
             {
                 UnityEngine.Debug.Log("YOU WIN!");
+                gameOverText.SetActive(true);
+                StartCoroutine(waitALittleBit());
+
                 UnityEngine.Debug.Log($"It took you {m_stopwatch.Elapsed} to find all {m_collectablesTotalCount} collectables.");
 
-#if UNITY_EDITOR
-                UnityEditor.EditorApplication.ExitPlaymode();
-#endif
+
 
             }
             else
@@ -74,10 +84,19 @@ public class PlayerController : MonoBehaviour
         else if(other.gameObject.CompareTag("Enemy"))
         {
             UnityEngine.Debug.Log("GAME OVER!");
+            gameOverText.SetActive(true);
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.ExitPlaymode();
 #endif
         }
+    }
+
+    public IEnumerator waitALittleBit()
+    {
+        yield return new WaitForSeconds(5);
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.ExitPlaymode();
+#endif
     }
 }
  
